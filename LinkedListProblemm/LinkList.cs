f9a79bb3cd2a045a89c09d9fc0147f465934d910
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-03 01:09:31 +0530

    [Sunny] Add : Adding the values In LinkList

---
+++ LinkedListProblemm/LinkList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblemm
+{
+    public class LinkList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted into Link List", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkList Is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListProblemm/Program.cs
@@ -0,0 +1,25 @@
+﻿using LinkedListProblemm;
+using System;
+namespace LinkedListProblem;
+class program
+{
+    public static void Main(string[] args)
+    {
+        LinkList list = new LinkList();
+        bool check = true;
+        Console.WriteLine("1.Simple Link List");
+        while (check)
+        {
+            Console.WriteLine("Take An Option To Execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    list.Add(30);
+                    list.Add(56);
+                    list.Add(70);
+                    break;
+            }
+        }
+    }
+}
