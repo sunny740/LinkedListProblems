271a29af6003818a6d9153612b0684012e7db215
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-03 12:44:15 +0530

    [Sunny] Add : Insert The Data Into Linked List

--- LinkedListProblemm/LinkList.cs
+++ LinkedListProblemm/LinkList.cs
@@ -8,8 +8,8 @@ namespace LinkedListProblemm
 {
     public class LinkList
     {
-        internal Node head;
-        internal void Add(int data)
+        public Node head;
+        public void Add(int data)
         {
             Node node = new Node(data);
             if (this.head == null)
@@ -27,7 +27,7 @@ namespace LinkedListProblemm
             }
             Console.WriteLine("{0} inserted into Linked list", node.data);
         }
-        internal void AddInReverseOrder(int data)
+        public void AddInReverseOrder(int data)
         {
             Node newNode = new Node(data);
             if (this.head == null)
@@ -41,18 +41,60 @@ namespace LinkedListProblemm
             }
             Display();
         }
-        internal void Display()
+        public void Display()
         {
             Node temp = this.head;
             if (temp == null)
             {
-                Console.WriteLine("List is Empty");
+                Console.WriteLine("Linked list is empty");
             }
             while (temp != null)
             {
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
+        }
+        public int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+        public Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
         }
     }
 }
--- LinkedListProblemm/Program.cs
+++ LinkedListProblemm/Program.cs
@@ -8,10 +8,10 @@ class program
         Console.WriteLine(" **** Welcome to Linked List Program ***\n");
         LinkList list = new LinkList();
         bool check = true;
-        Console.WriteLine("1.Simple Linked List\n2.Add in Reverse Order");
+        Console.WriteLine("1.Simple Linked List\n2.Add in reverse order\n3.Insert number at particular position");
         while (check)
         {
-            Console.WriteLine("Take An Option To Execute");
+            Console.WriteLine("Take an option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,13 @@ class program
                     list.AddInReverseOrder(56);
                     list.AddInReverseOrder(30);
                     list.AddInReverseOrder(70);
+                    break;
+                case 3:
+                    list.Add(56);
+                    list.Add(70);
+                    int a = list.Search(56);
+                    list.InsertAtParticularPosition(a + 1, 30);
+                    list.Display();
                     break;
             }
         }
